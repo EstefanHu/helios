0cf5e43f0ca40346805583798b3666156734913b
Estefan Hu <estefanhu074@gmail.com>
2023-11-24 12:55:00 -0800

    fuck off gpt

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,10 +1,16 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/app/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/app/',
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: '/',
+      },
+    ],
     sitemap: 'https://helios-journal/sitemap.xml',
   };
 }
