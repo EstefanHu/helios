16b870988b758d89ce510cf850e0b3bcc59c5018
Estefan Hu <estefanhu074@gmail.com>
2023-12-07 22:46:25 -0800

    lint

--- src/lib/config/db.js
+++ src/lib/config/db.js
@@ -3,12 +3,12 @@ import { Pool } from 'pg';
 let db;
 
 if (!db) {
-    db = new Pool({ connectionString: process.env.POSTGRES_URL });
+  db = new Pool({ connectionString: process.env.POSTGRES_URL });
 }
 
 db.on('error', (err, db) => {
-    console.error('Unexpected error on idle client', err)
-    process.exit(-1)
-})
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
 export default db;
