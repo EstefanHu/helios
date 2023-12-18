48fe65921a5d7d79e76555ba4c13ed1c96d41665
Misty <60805050+mistyb01@users.noreply.github.com>
2023-12-18 12:30:58 -0800

    Create components for entry list UI (#4)
    * set up entry list components
    
    * style entry date box
    
    * style entry list item
    
    * add spacing in entry list container
    
    * simplify list item component, create month wrapper
    
    * dynamic month wrapper
    
    * dynamic entry list items
    
    * fix time format
    
    * minor styling edits
    
    * create static search menu
    
    * move search bar code to its own component
    
    * add truncation to entry previews
    
    * small css edits to search bar
    
    * sticky positioned month headings
    
    * try to fix lint error
    
    * sort entries reverse chronologically
    
    * increase line height of body text
    
    * add bottom padding to page container
    
    * remove sass build files

---
+++ src/app/(app)/home/EntryListItem.jsx
@@ -0,0 +1,29 @@
+'use client';
+import styles from './home.module.scss';
+
+export default function EntryListItem({ entry }) {
+  return (
+    <div className={styles.entryDayWrapper}>
+      <div className={styles.dateBox}>
+        <p className={styles.dateBoxNumber}>{entry.creationDateTime.toLocaleString('default', { day: 'numeric' })}</p>
+        <p className={styles.dateBoxWeekday}>
+          {entry.creationDateTime.toLocaleString('default', { weekday: 'short' })}
+        </p>
+      </div>
+      <div className={styles.entryDayList}>
+        <div className={styles.entryListItem}>
+          <p className={styles.entryListItemTime}>
+            {Intl.DateTimeFormat('en', { hour: 'numeric', minute: 'numeric', hour12: true }).format(
+              entry.creationDateTime
+            )}
+          </p>
+
+          <h2 className={styles.entryListItemTitle}>{entry.title}</h2>
+          <div className={styles.entryListItemBody}>
+            <p>{entry.body}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/(app)/home/page.jsx
+++ src/app/(app)/home/page.jsx
@@ -1,7 +1,104 @@
+import EntryListItem from './EntryListItem';
+import EntryMonthWrapper from './EntryMonthWrapper';
+import SearchFilterContainer from './SearchFilterContainer';
+import styles from './home.module.scss';
+
+const mockEntries = [
+  {
+    id: 0,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('October 31, 2023 18:30:00'),
+    updatedDateTime: new Date('December 8, 2023 18:30:00'),
+    tags: ['peaceful', 'evening', 'reflection'],
+  },
+  {
+    id: 1,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('December 8, 2023 18:30:00'),
+    updatedDateTime: new Date('December 8, 2023 18:30:00'),
+    tags: ['peaceful', 'evening', 'reflection'],
+  },
+  {
+    id: 2,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('December 7, 2023 21:15:00'),
+    updatedDateTime: new Date('December 7, 2023 21:15:00'),
+    tags: ['city lights', 'adventure', 'friends'],
+  },
+  {
+    id: 3,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('December 5, 2023 19:00:00'),
+    updatedDateTime: new Date('December 5, 2023 19:00:00'),
+    tags: ['thanksgiving', 'family', 'gratitude'],
+  },
+  {
+    id: 4,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('November 18, 2023 14:45:00'),
+    updatedDateTime: new Date('November 18, 2023 14:45:00'),
+    tags: ['rainy day', 'introspection', 'coziness'],
+  },
+  {
+    id: 5,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('November 5, 2023 11:00:00'),
+    updatedDateTime: new Date('November 5, 2023 11:00:00'),
+    tags: ['autumn', 'nature', 'inspiration'],
+  },
+  {
+    id: 6,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('October 24, 2023 11:00:00'),
+    updatedDateTime: new Date('November 5, 2023 11:00:00'),
+    tags: ['autumn', 'nature', 'inspiration'],
+  },
+  {
+    id: 7,
+    title: 'Title',
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique eos nobis voluptatum quibusdam aspernatur animi dignissimos fugit reprehenderit, delectus perferendis cupiditate laudantium laboriosam ullam dolore quo, deleniti saepe omnis. Id sed nam, omnis similique deleniti quo esse architecto, nulla cum ex quidem. Quam esse doloribus tempora perferendis, minima possimus, aliquam voluptates odio ab, rem provident unde amet magnam fugiat facilis pariatur deleniti non consectetur vel libero nostrum blanditiis totam debitis accusantium. Dicta corrupti labore consequatur, vel velit voluptate est eum sint optio. Autem porro corporis ea consectetur numquam neque minima ducimus praesentium qui velit iste vitae voluptate, eum quae necessitatibus?',
+    creationDateTime: new Date('November 12, 2023 11:00:00'),
+    updatedDateTime: new Date('November 5, 2023 11:00:00'),
+    tags: ['autumn', 'nature', 'inspiration'],
+  },
+];
+
 export default function Home() {
+  let reverseChronoEntries = mockEntries.sort((a, b) => b.creationDateTime - a.creationDateTime);
+
+  // use a set to obtain a list of the months that have an entry,
+  // for use when mapping to EntryMonthWrapper
+  const monthSet = new Set();
+  reverseChronoEntries.forEach((entry) =>
+    monthSet.add(entry.creationDateTime.toLocaleString('default', { month: 'long' }))
+  );
+  const entryMonths = [...monthSet];
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.homeContainer}>
+      <SearchFilterContainer />
+      <div className={styles.entryListContainer}>
+        {entryMonths.map((month) => {
+          const filteredEntriesByMonth = reverseChronoEntries.filter(
+            (entry) => entry.creationDateTime.toLocaleString('default', { month: 'long' }) === month
+          );
+
+          return (
+            <EntryMonthWrapper key={month} month={month}>
+              {filteredEntriesByMonth.map((entry) => (
+                <EntryListItem key={entry.id} entry={entry} />
+              ))}
+            </EntryMonthWrapper>
+          );
+        })}
+      </div>
     </div>
   );
 }
