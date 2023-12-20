aa660cb2f443a0e529db4cf58f3e2fd8eb06b253
Estefan Hu <estefanhu074@gmail.com>
2023-12-19 22:19:23 -0800

    Removed modal menus
    * Removed profile menu
    * Removed notifications menu

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # The Helios Project
 
 ## Postgresql
+
 ### Seed data
+
 1. make sure local postgres instance is running
 2. run `npm run postges::seed`
 3. for first time users answer `y` to both questions
--- src/app/(app)/AppLayoutClientComponents.jsx
+++ src/app/(app)/AppLayoutClientComponents.jsx
@@ -1,7 +1,5 @@
 'use client';
 import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect, useRef } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 
 import { HiSun } from 'react-icons/hi';
@@ -9,68 +7,8 @@ import { BiSolidHomeHeart } from 'react-icons/bi';
 import { GiJourney, GiMirrorMirror } from 'react-icons/gi';
 import { MdPersonOutline } from 'react-icons/md';
 import { IoIosMore } from 'react-icons/io';
-import { FaBell } from 'react-icons/fa';
-import { FiMenu } from 'react-icons/fi';
-import { BsBellSlash } from 'react-icons/bs';
 
 import styles from './AppLayoutClientComponents.module.scss';
-
-function AppLinks() {
-  const notificationButtonRef = useRef(null);
-  const menuButtonRef = useRef(null);
-  const mobileMenuButtonRef = useRef(null);
-  const router = useRouter();
-  const imageUrl = false;
-
-  useEffect(() => {
-    const notifications = document.getElementById('notificationsMenu');
-    const menu = document.getElementById('menu');
-
-    const toggleMenus = (e) => {
-      if (e.target === notificationButtonRef.current) {
-        menu.classList.remove('visible');
-        notifications.classList.toggle('visible');
-      } else if (e.target === menuButtonRef.current || e.target === mobileMenuButtonRef.current) {
-        notifications.classList.remove('visible');
-        menu.classList.toggle('visible');
-      } else if (e.target !== notifications && e.target !== menu.firstChild) {
-        menu.classList.remove('visible');
-        notifications.classList.remove('visible');
-      }
-    };
-    document.addEventListener('click', toggleMenus);
-
-    return () => {
-      document.removeEventListener('click', toggleMenus);
-    };
-  }, []);
-
-  return (
-    <span className={styles.appLinks}>
-      <button type='button' onClick={() => router.push('/write')} className={styles.headerLinkWriteButton}>
-        write
-      </button>
-
-      <button type='button' ref={notificationButtonRef}>
-        <FaBell />
-      </button>
-
-      <button type='button'>
-        <Image
-          src={imageUrl || '/images/defaultProfileImage.webp'}
-          ref={menuButtonRef}
-          fill
-          sizes='100%'
-          alt='profile image'
-        />
-      </button>
-
-      <button type='button' ref={mobileMenuButtonRef} className={styles.mobile}>
-        <FiMenu />
-      </button>
-    </span>
-  );
-}
 
 function AppNav() {
   const router = useRouter();
@@ -117,33 +55,6 @@ function AppNav() {
   );
 }
 
-const DEFAULT_SEEKER = {
-  name: '',
-  emailAddress: '',
-  imageUrl: '',
-};
-
-function MenuSeekerDetails({ getSeeker }) {
-  const [seeker, setSeeker] = useState(DEFAULT_SEEKER);
-
-  if (!seeker) return null;
-
-  return (
-    <Link href='/profile'>
-      <div className={styles.MenuSeekerDetails}>
-        <div className={styles.ImageWrapper}>
-          <Image src={seeker.imageUrl || '/images/defaultProfileImage.webp'} alt='profile picture' sizes='100%' fill />
-        </div>
-
-        <div className={styles.profileInfo}>
-          <p className={styles.primary}>{seeker.name}</p>
-          <p className={styles.secondary}>{seeker.emailAddress}</p>
-        </div>
-      </div>
-    </Link>
-  );
-}
-
 function MobileAppNav() {
   const pathname = usePathname();
 
@@ -168,44 +79,10 @@ function MobileAppNav() {
   );
 }
 
-function Notifications() {
-  const [notifications, setNotifications] = useState([]);
-  // fetch notifications
-
-  if (notifications.length === 0)
-    return (
-      <div className={styles.noNotifications}>
-        <BsBellSlash />
-        <p>You have any notifications</p>
-      </div>
-    );
-
-  return (
-    <div className={styles.notifications}>
-      <p>notifications</p>
-    </div>
-  );
-}
-
 function PageName() {
   const pageName = usePathname()?.split('/')[1];
 
   return <h1 className={styles.pageName}>{pageName}</h1>;
 }
 
-export default function SignOut() {
-  const router = useRouter();
-
-  const signOut = async () => {
-    await fetch('/api/auth', { method: 'DELETE' });
-    router.push('/');
-  };
-
-  return (
-    <button type='button' onClick={signOut} className={styles.signout}>
-      Sign out
-    </button>
-  );
-}
-
-export { AppLinks, AppNav, MenuSeekerDetails, MobileAppNav, Notifications, PageName, SignOut };
+export { AppNav, MobileAppNav, PageName };
