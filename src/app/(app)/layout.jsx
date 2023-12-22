7a0c6dc9e0d45461a3ad2091b3324a6b6b4c2851
Estefan <estefanhu074@gmail.com>
2023-12-21 23:22:24 -0800

    Removed modal menus (#7)
    * Removed profile menu
    * Removed notifications menu

--- src/app/(app)/layout.jsx
+++ src/app/(app)/layout.jsx
@@ -1,19 +1,5 @@
 import Link from 'next/link';
-
-import { BiSolidHomeHeart } from 'react-icons/bi';
-import { GiJourney, GiMirrorMirror } from 'react-icons/gi';
-import { FiInbox } from 'react-icons/fi';
-
-import {
-  AppLinks,
-  AppNav,
-  MenuSeekerDetails,
-  MobileAppNav,
-  Notifications,
-  PageName,
-  SignOut,
-} from './AppLayoutClientComponents';
-
+import { AppNav, MobileAppNav, PageName } from './AppLayoutClientComponents';
 import styles from './layout.module.scss';
 
 export default function AppLayout({ children }) {
@@ -32,73 +18,15 @@ export default function AppLayout({ children }) {
         <header>
           <PageName />
 
-          <AppLinks />
+          <Link href='/write'>
+            <p>write</p>
+          </Link>
         </header>
 
         <div className={styles.contentWrapper}>
           <div className={styles.content}>{children}</div>
         </div>
       </main>
-
-      <div className={styles.appheaderMenuWrapper} id='menu'>
-        <div className={styles.appHeaderMenu}>
-          <MenuSeekerDetails getSeeker={getSeeker} />
-
-          <hr />
-
-          <div className={styles.menuLinks}>
-            <Link href='/home' className={styles.link}>
-              <BiSolidHomeHeart />
-              <p>home</p>
-            </Link>
-
-            <Link href='/journey' className={styles.link}>
-              <GiJourney />
-              <p>journey</p>
-            </Link>
-
-            <Link href='/reflect' className={styles.link}>
-              <GiMirrorMirror />
-              <p>reflect</p>
-            </Link>
-
-            <Link href='/inbox' className={styles.link}>
-              <FiInbox />
-              <p>inbox</p>
-            </Link>
-          </div>
-
-          <hr />
-
-          <div className={styles.menuLinks}>
-            <Link href='/' className={styles.link}>
-              <p className={styles.light}>settings</p>
-            </Link>
-
-            <SignOut />
-          </div>
-
-          <hr />
-
-          <span className={styles.legalLinks}>
-            <Link href='/privacy'>Privacy</Link>
-            <Link href='/tos'>Terms</Link>
-            <Link href='/ccpa'>Collection notice</Link>
-          </span>
-        </div>
-      </div>
-
-      <div className={styles.notificationsMenu} id='notificationsMenu'>
-        <div className={styles.notificationsHeader}>
-          <p>Notifications</p>
-        </div>
-
-        <Notifications />
-
-        <div className={styles.notificationsButtonWrapper}>
-          <Link href='/notifications'>See all</Link>
-        </div>
-      </div>
 
       <MobileAppNav />
     </div>
