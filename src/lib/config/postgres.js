114bea0b45cfa6cea444126a7a7944a2dd3d0377
Estefan Hu <estefanhu074@gmail.com>
2024-01-08 20:48:27 -0800

    Bugfix: Migrated POSTGRES => DB

--- src/lib/config/postgres.js
+++ src/lib/config/postgres.js
@@ -7,7 +7,7 @@ if (!global.db) {
 export function connectToDatabase() {
   if (!global.db.pool) {
     console.log('No pool available, creating new pool.');
-    global.db.pool = new Pool({ connectionString: process.env.POSTGRES_URL });
+    global.db.pool = new Pool({ connectionString: process.env.DB_URL });
   }
 
   return global.db;
