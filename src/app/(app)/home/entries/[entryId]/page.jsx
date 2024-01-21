6e26b583779eab2c6c2e2f5e8a34f5424945bfca
Misty <mistyb140@gmail.com>
2024-01-21 14:47:43 -0800

    basic styling for entry viewer

--- src/app/(app)/home/entries/[entryId]/page.jsx
+++ src/app/(app)/home/entries/[entryId]/page.jsx
@@ -1,13 +1,27 @@
- 
 import { getEntry } from "@/app/actions/entries"
+import Link from "next/link"
+import styles from './viewer.module.scss';
+
 
 export default async function Page({ params }) {
   const entry = await getEntry(params.entryId)
   return (
-    <>
-    <h1>{entry[0].title}</h1>
-    <br />
-    <p>{entry[0].body}</p>
-    </>
+    <section className={styles.viewerContainer}>
+      <div className="viewer-nav">
+        {/* <Link href="/home">back to home</Link> */}
+      </div>
+      
+      <section className={styles.viewerDetails}>
+          <span>{entry[0].created_at.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</span>
+        </section>
+
+      <section className={styles.viewerHeader}>
+        <h1 className={styles.title}>{entry[0].title}</h1>
+      </section>
+
+      <section className={styles.body}>
+        <p>{entry[0].body}</p>
+      </section>
+    </section>
   )
 }
