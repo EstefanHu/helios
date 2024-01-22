8468000e954580ed242f7abab72817c7e683876b
Misty <mistyb140@gmail.com>
2024-01-21 16:06:27 -0800

    style date and edit button

--- src/app/(app)/home/entries/[entryId]/page.jsx
+++ src/app/(app)/home/entries/[entryId]/page.jsx
@@ -8,12 +8,19 @@ export default async function Page({ params }) {
   return (
     <section className={styles.viewerContainer}>
       <div className="viewer-nav">
-        {/* <Link href="/home">back to home</Link> */}
+        {/* <Link href="/home">back to list</Link> */}
       </div>
       
       <section className={styles.viewerDetails}>
-          <span>{entry[0].created_at.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</span>
-        </section>
+          <div className={styles.createdTimeDate}>
+            <span className={styles.date}>{entry[0].created_at.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</span>|
+            <span className={styles.date}>{entry[0].created_at.toLocaleTimeString('en-us', { timeStyle: 'short' })}</span>
+          </div>
+          <div className={styles.edit}>
+            <button className={styles.editButton}>Edit</button>
+            <span className={styles.editedText}>last edited {entry[0].updated_at.toLocaleDateString('en-us', { month:"short", day:"numeric"})}</span>
+          </div>
+      </section>
 
       <section className={styles.viewerHeader}>
         <h1 className={styles.title}>{entry[0].title}</h1>
