db894d4da47ad05dc569fd6e1eb890063ff3359c
Misty <mistyb140@gmail.com>
2024-02-24 11:04:19 -0800

    return response codes in entry queries

--- src/app/actions/entries.js
+++ src/app/actions/entries.js
@@ -6,9 +6,8 @@ const { pool } = connectToDatabase();
 export async function getEntry(slug) {
   const client = await pool.connect();
   try {
-    const res = await client.query('SELECT * FROM entry WHERE slug = $1', [slug]);
-
-    return res.rows;
+    const { rows } = await client.query('SELECT * FROM entry WHERE slug = $1', [slug]);
+    return { code: 200, entry: rows[0] }
   } catch (error) {
     console.log(error);
   } finally {
@@ -43,11 +42,10 @@ export async function getEntries(travelerId, limit, offset) {
   const client = await pool.connect();
   const query = 'SELECT * FROM entry WHERE traveler_id = $1 ORDER BY created_at DESC LIMIT $2 OFFSET $3';
   try {
-    const res = await client.query(query, [travelerId, limit, offset]);
-
-    return res.rows;
+    const { rows } = await client.query(query, [travelerId, limit, offset]);
+    return { code: 200, entries: rows[0] };
   } catch (error) {
-    console.log(error);
+    return { error }, { status: 500 }
   } finally {
     client.release();
   }
@@ -56,11 +54,10 @@ export async function getEntries(travelerId, limit, offset) {
 export async function getEntryCount(travelerId) {
   const client = await pool.connect();
   try {
-    const res = await client.query('SELECT count(*) FROM entry WHERE traveler_id = $1', [travelerId]);
-
-    return res.rows[0];
+    const { rows } = await client.query('SELECT count(*) FROM entry WHERE traveler_id = $1', [travelerId]);
+    return { code: 200, count: rows[0] };
   } catch (error) {
-    console.log(error);
+    return { error }, { status: 500 }
   } finally {
     client.release();
   }
